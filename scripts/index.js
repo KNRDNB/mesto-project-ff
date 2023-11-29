ac63ade015af651e051a2eb304e6321881c17529
Aleksandr Bondarenko <alexander.d.bondarenko@gmail.com>
2023-11-29 17:19:29 +0300

    added js code

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,24 @@
-// @todo: Темплейт карточки
+const cardTmp = document.querySelector('#card-template').content;
+const cardContainer = document.querySelector('.places__list');
 
-// @todo: DOM узлы
+const deleteCard = function (evt) {
+  evt.target.parentNode.remove();
+};
 
-// @todo: Функция создания карточки
+initialCards.forEach((item) => {
+  addCards(item.name, item.link, deleteCard);
+});
 
-// @todo: Функция удаления карточки
+function addCards(name, link, del) {
+  const cardElement = cardTmp.querySelector('.card').cloneNode(true);
+  const cardImg = cardElement.querySelector('.card__image');
+  const cardDeleteBtn = cardElement.querySelector('.card__delete-button');
 
-// @todo: Вывести карточки на страницу
+  cardElement.querySelector('.card__title').textContent = name;
+  cardImg.setAttribute('src', link);
+  cardImg.setAttribute('alt', 'Картинка карточки');
+  cardDeleteBtn.addEventListener('click', del);
+
+  cardContainer.append(cardElement);
+  return cardElement;
+}
